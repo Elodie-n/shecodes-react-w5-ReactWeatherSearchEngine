cd3c800f18334766faaf41bbffa6ceb7c05fe574
Elodie-n <94834435+Elodie-n@users.noreply.github.com>
2022-02-10 19:13:04 +0100

    Added a search engine to the app and live data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import Weather from "./Weather";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <Weather defaultCity="Paris" />
+        <footer className="text-muted">
+          This project was coded by{" "}
+          <span className="myName">Elodie Nerjat</span> and is{" "}
+          <a
+            href="https://github.com/Elodie-n/shecodes-react-w5-ReactWeatherSearchEngine"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://vibrant-clarke-4d6453.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
